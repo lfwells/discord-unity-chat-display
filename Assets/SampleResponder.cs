09d34b2ada8921bc1b6c0406c3e4f16fb3e916e3
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-01-03 18:04:51 +1100

    refactored the sample responder a little

--- Assets/SampleResponder.cs
+++ Assets/SampleResponder.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 
+using DiscordUnityChatDisplay;
+
 public class SampleResponder : MonoBehaviour
 {
     public GameObject testBall;
-    public void OnChannelName(DiscordUnityChatDisplay.ChannelUpdateEvent evt)
+    public InteractionOption[] optionsTest;
+    public void OnChannelName(ChannelUpdateEvent evt)
     {
         Debug.Log("got channel "+evt.name);
     }
-    public void OnInteractionCreate(DiscordUnityChatDisplay.InteractionCreateEvent evt)
+    public void OnInteractionCreate(InteractionCreateEvent evt)
     {
         //Debug.Log(evt.customId);
         //Debug.Log(evt.member.name);
+
+        optionsTest = evt.options;
 
         Debug.Log(evt.member.avatar);
         var go = Instantiate(testBall, transform.position, transform.rotation, transform);
