b10de134f50ba10117faa1db67db438e74e932eb
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-01-03 17:54:35 +1100

    Created a poll responder object and script

---
+++ Assets/PollResponder.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PollResponder : MonoBehaviour
+{
+    public void OnInteractionCreate(DiscordUnityChatDisplay.InteractionCreateEvent evt)
+    {
+        
+    }
+}
