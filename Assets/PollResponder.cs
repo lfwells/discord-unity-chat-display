b9ece486bfee486b86a51f7e3805d7d62c0df0b3
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-01-03 18:11:58 +1100

    Added basic structure of the poll class

--- Assets/PollResponder.cs
+++ Assets/PollResponder.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DiscordUnityChatDisplay;
+
+//current limitations: only the most recent poll will appear, all votes for previous polls will be ignored
 
 public class PollResponder : MonoBehaviour
 {
+    [System.Serializable]
+    public class Poll
+    {
+        public string interactionId;
+        public string question;
+        public string[] answers;
+        public Dictionary<string, DiscordMember[]> votes;
+    }
+
+    public Poll currentPoll;
+
     public void OnInteractionCreate(DiscordUnityChatDisplay.InteractionCreateEvent evt)
     {
-        
+        //if incoming command
+        if ("poll" == evt.commandName)
+        {
+            currentPoll = new Poll {
+                interactionId = evt.id
+            };
+        }
     }
 }
