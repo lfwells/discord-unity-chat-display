ec1cb2d5e5ef32add7d5cb0389bac0d56022706d
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-01-04 12:29:08 +1100

    Created a poll visuals prefab and script

---
+++ Assets/PollVisuals.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PollVisuals : MonoBehaviour
+{
+    public GameObject pollBucketPrefab;
+
+    List<PollBucket> buckets;
+
+
+}
