ab876bd49b1a01f31db22ad0778aabe8718de302
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-03-03 17:24:57 +1100

    save channel id to player prefs to save time

--- Assets/ConnectToChannel.cs
+++ Assets/ConnectToChannel.cs
@@ -6,16 +6,29 @@ using UnityEngine.SceneManagement;
 public class ConnectToChannel : MonoBehaviour
 {
     public int sceneBuildIndex = 1;
+
+    //pre-fill the channel id from playerpref in the text box 
+    private void Start()
+    {
+        var input = GetComponentInChildren<TMPro.TMP_InputField>();
+        input.text = PlayerPrefs.GetString("channelID");
+    }
+
     public void Connect(string channelID)
     {
         StartCoroutine(LoadScene(channelID));
     }
     IEnumerator LoadScene(string channelID)
     {
+        //save the channel id to playerprefs
+        PlayerPrefs.SetString("channelID", channelID);
+
         var op = SceneManager.LoadSceneAsync(sceneBuildIndex, LoadSceneMode.Additive);
         yield return op;
+
         var chat = FindObjectOfType<DiscordUnityChatDisplay.DiscordChat>();
         chat.channelID = channelID;
+
         Debug.Log("set channel id "+channelID);
         Destroy(transform.root.gameObject);
 
