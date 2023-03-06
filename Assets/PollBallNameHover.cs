584262e8d3003c6325ad57cb9d49a477112fe85c
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-03-06 16:59:45 +1100

    added text box link to script, added functionality for hiding showing and setting text

--- Assets/PollBallNameHover.cs
+++ Assets/PollBallNameHover.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PollBallNameHover : MonoBehaviour
 {
+    public TMPro.TextMeshPro nameText;
+
+    bool _visible = false;
+    public bool Visible
+    {
+        get { return _visible; }
+        set
+        { 
+            _visible = value;
+            nameText.enabled = value;
+        }
+    }
+
+    string _name;
+    public string Name
+    {
+        get { return _name; }
+        set
+        {
+            _name = value;
+            nameText.text = value;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Visible = false;
+        Name = "";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
